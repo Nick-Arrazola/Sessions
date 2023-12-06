d9c5ccd1f443f9e67244c2853f0c8fee386c2564
Nicolas Arrazola <narrazol@terpmail.umd.edu>
2023-12-05 19:49:07 -0500

    Created the form and updated font

--- webServer.js
+++ webServer.js
@@ -1,15 +1,17 @@
-const express = require('express'),
+const body_parser = require("body-parser"),
+      express = require('express'),
       path = require('path');
 
 const app = express();
     //   , DOMAIN = "localhost:5000";
 
 
-
-
 /* Sets destination of where the templates are and specifies which type of engine we'll use */
 app.set("views", path.resolve(__dirname, "./templates"));
 app.set("view engine", "ejs");
+
+ /* Use this middleware to read POST body */
+ app.use(body_parser.urlencoded({extended: false}))
 
 /* This endpoint handles all of the css GET requests */
 app.get('/assets/:stylesheet', (req, res) => {
@@ -29,7 +31,25 @@ app.get("/join", (req, res) => {
     res.render("joinPage");
 });
 
+app.get("/join/private", (req, res) => {
+
+    let sessionID = req.query.sessionID;
+
+    /* TODO: retrieve the session with this ID from the database */
+
+    res.render("session");
+});
+
 app.get("/create", (req, res) => {
+
+    res.render("createConfig");
+});
+
+app.post("/create/done", (req, res) => {
+
+    console.log(req.body)
+
+    /* TODO: here, extract the actual content of the POST request and store it in DB */
 
     res.render("sessions");
 });
